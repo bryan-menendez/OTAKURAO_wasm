f77a65c91c5fe8471da824227c5c8287e547cab2
bryan_menendez <bryan.menendez@protonmail.com>
2021-03-08 19:19:31 -0300

    initial commit

---
+++ OTAKURAO_wasm/Pages/Game.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace OTAKURAO_wasm.Pages
+{
+    public partial class Game
+    {
+        public Game()
+        {
+
+        }
+    }
+}
---
+++ OTAKURAO_wasm/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace OTAKURAO_wasm.Pages
+{
+    public partial class Index
+    {
+        private int currentCount = 0;
+
+        public Index()
+        {
+
+        }
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
